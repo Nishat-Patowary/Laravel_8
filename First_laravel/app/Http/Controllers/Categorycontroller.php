869f2c3c6f6e2527996255a5f9705b9a5f70ac78
Nishat-patowary <nishatpatwary2001@gmail.com>
2021-10-25 22:16:24 +0600

    first_laravel

--- First_laravel/app/Http/Controllers/Categorycontroller.php
+++ First_laravel/app/Http/Controllers/Categorycontroller.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class Categorycontroller extends Controller
 {
-    public function Allcat(){
+    public function AllCat(){
         return view('admin.category.index');
     }
 }
