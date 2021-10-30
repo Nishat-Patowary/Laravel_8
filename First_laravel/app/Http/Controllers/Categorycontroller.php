71c0662c66620a4dead331b12266b51219a98f14
Nishat-patowary <nishatpatwary2001@gmail.com>
2021-10-30 12:37:50 +0600

    first_laravel

--- First_laravel/app/Http/Controllers/Categorycontroller.php
+++ First_laravel/app/Http/Controllers/Categorycontroller.php
@@ -59,22 +59,29 @@ class Categorycontroller extends Controller
     }
 
   public function Edit($id){
-      $categories = category::find($id);
+      //$categories = category::find($id);
+      $categories = DB::table('categories')->where('id',$id)->first();
+      
       return view('admin.category.edit',compact('categories'));
 
   }
 
-  //public function Update(Request $request, $id){
+  public function Update(Request $request, $id){
     //  $update = Category::find($id)->update([
     //    'category_name' => $request->category_name,
      //   'user_id' => Auth::user()->id 
 
      // ]);
 
-    //  return Redirect()->route('all.category')->with('success','Category Updated Successfully');
+     $data = array();
+     $data['category_name'] = $request->category_name;
+     $data['user_id'] = Auth::user()->id;
+     Db::table('categories')->where('id',$id)->update($data);
+
+     return Redirect()->route('all.category')->with('success','Category Updated Successfully');
 
       
- // }
+  }
   
     
-}
+    }
